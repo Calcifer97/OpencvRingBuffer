64a73f489632d206be459204b96ad515548c1970
zhangweijie97 <37408196+zhangweijie97@users.noreply.github.com>
2020-02-07 12:57:25 +0800

    Update OpencvRingBuffer-test.py

--- OpencvRingBuffer-test.py
+++ OpencvRingBuffer-test.py
@@ -3,23 +3,23 @@ import time
 from OpencvRingBuffer import OpencvRingBuffer
 
 cap = cv2.VideoCapture(0)
-
 buff=OpencvRingBuffer(cap)#建立OpencvRingBuffer
 buff.startcap()#开启线程捕捉
+time.sleep(2)#摄像头热身
 
-time.sleep(1)#假如你的算法导致1s延迟
 ret,frame=buff.getnew()#获得最新帧
 print(time.time())
 cv2.imwrite("captest1.jpg", frame)
-
 time.sleep(1)#假如你的算法导致1s延迟
+
 ret,frame=buff.getnew()
 print(time.time())
 cv2.imwrite("captest2.jpg", frame)
-
 time.sleep(1)#假如你的算法导致1s延迟
+
 ret,frame=buff.getnew()
 print(time.time())
 cv2.imwrite("captest3.jpg", frame)
+time.sleep(1)#假如你的算法导致1s延迟
 
 buff.stopcap()
